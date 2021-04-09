22029ad67289986d121f9ad536e0c7723d8afd22
rajktmrider <mrajcivil96@gmail.com>
2021-04-09 12:37:31 +0530

    sample git2

---
+++ src/test/java/com/test/GitSample2.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class GitSample2 {
+ private void sample1() {
+	 System.out.println("sample1");
+	
+}
+ private void sample2() {
+	System.out.println("sample2");
+}
+ private void sample3() {
+	System.out.println("sample3");
+}
+}
